43c6af8df15f7f63cf65f71bedfd633da2af4efc
Nicolas Cordin <dindonus@users.noreply.github.com>
2018-08-04 17:30:53 +0200

    Add minimal Electron boiterplate

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, Electron!</h1>
+        <p>Ready to fly?</p>
+      </div>
+    );
+  }
+}
+
+export default App;
